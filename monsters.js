6a5743c8450a7947853c2b9d74da0232525fea99
DansonVuong <70728975+DansonVuong@users.noreply.github.com>
2020-09-24 20:52:54 -0500

    Add files via upload

---
+++ monsters.js
@@ -0,0 +1,17 @@
+var listOfMonsters= [
+			{
+						name: "Slime",
+						hp: 15,
+						damage: 8
+			},
+			{
+						name: "Zombie",
+						hp: 45,
+						damage: 12
+			},
+			{
+						name: "Orc",
+						hp: 80,
+						damage: 20
+			}	
+]
